ee48b001fd91e51fc57e952b344cd72f0b059062
Данила Беляков <danilabelakov@MacBook-Air-von-Danila.local>
2024-07-30 23:49:57 +0300

    Added examples of buttons and cards

---
+++ examples/src/main/kotlin/com/github/examples/Button.kt
@@ -0,0 +1,43 @@
+package com.github.examples
+
+import com.github.alice.ktx.dispatch
+import com.github.alice.ktx.handlers.message
+import com.github.alice.ktx.models.button.button
+import com.github.alice.ktx.models.response.response
+import com.github.alice.ktx.server.impl.ktorWebServer
+import com.github.alice.ktx.skill
+
+private enum class SchedulesType(val title: String) {
+    LAST_DAY("Последний день"),
+    TODAY("Сегодня"),
+    NEXT_DAY("Следующий день")
+}
+
+fun main() {
+    skill {
+        id = "..."
+        webServer = ktorWebServer {
+            port = 8080
+            path = "/alice"
+        }
+        dispatch {
+            message({ message.session.new }) {
+                response {
+                    text = "Выберите тип"
+                    SchedulesType.entries.forEach {
+                        button {
+                            text = it.name
+                            payload = mapOf("schedule_type" to it.name)
+                        }
+                    }
+                }
+            }
+            message({ message.request.payload?.keys?.contains("schedule_type") == true }) {
+                val scheduleType = SchedulesType.valueOf(message.request.payload?.get("schedule_type")!!)
+                response {
+                    text = "Result: ${scheduleType.title}"
+                }
+            }
+        }
+    }.run()
+}
---
+++ examples/src/main/kotlin/com/github/examples/Cards.kt
@@ -0,0 +1,70 @@
+package com.github.examples
+
+import com.github.alice.ktx.dispatch
+import com.github.alice.ktx.handlers.message
+import com.github.alice.ktx.models.button.mediaButton
+import com.github.alice.ktx.models.card.*
+import com.github.alice.ktx.models.response.response
+import com.github.alice.ktx.server.impl.ktorWebServer
+import com.github.alice.ktx.skill
+
+// Замените идентификатор изображения на свой собственный
+private const val IMAGE_ID = "1521359/48faa5e0f3d3842a6329"
+
+fun main() {
+    skill {
+        id = "..."
+        webServer = ktorWebServer {
+            port = 8080
+            path = "/alice"
+        }
+        dispatch {
+            message({ message.request.command == "card_items_list" }) {
+                response {
+                    text = "CARD ITEMS LIST"
+                    cardItemsList {
+                        header = "HEADER"
+                        repeat(10) { index ->
+                            item {
+                                imageId = IMAGE_ID
+                                title = "#${index + 1}"
+                            }
+                        }
+                        footer {
+                            text = "Footer text"
+                            mediaButton {
+                                text = "Click"
+                            }
+                        }
+                    }
+                }
+            }
+
+            message({ message.request.command == "card_big_image" }) {
+                response {
+                    cardBigImage {
+                        imageId = IMAGE_ID
+                        title = "CARD BIG IMAGE"
+                        mediaButton {
+                            text = "Open url"
+                            url = "https://ya.ru"
+                        }
+                    }
+                }
+            }
+
+            message({ message.request.command == "card_image_gallery" }) {
+                response {
+                    cardImageGallery {
+                        repeat(10) { index ->
+                            item {
+                                imageId = IMAGE_ID
+                                title = "#${index + 1}"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }.run()
+}
