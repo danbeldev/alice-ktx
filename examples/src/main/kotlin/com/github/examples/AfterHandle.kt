bcb4365512a0d7345c2692537523c62121b8ae02
Данила Беляков <danilabelakov@MacBook-Air-Danila.local>
2025-07-29 23:17:40 +0400

    after handle add examples and tests

--- examples/src/main/kotlin/com/github/examples/AfterHandle.kt
+++ examples/src/main/kotlin/com/github/examples/AfterHandle.kt
@@ -1,4 +1,33 @@
 package com.github.examples
 
-class AfterHandle {
+import com.github.alice.ktx.dispatch
+import com.github.alice.ktx.handlers.impl.request
+import com.github.alice.ktx.middleware.afterHandle
+import com.github.alice.ktx.models.response.response
+import com.github.alice.ktx.skill
+import com.github.alice.ktx.webhook.impl.ktorWebhookServer
+
+fun main() {
+    skill {
+        webhookServer = ktorWebhookServer {
+            port = 8080
+            path = "/alice"
+        }
+
+        dispatch {
+            afterHandle { processRequestEnvironment, messageResponse ->
+                messageResponse.copy(
+                    response = messageResponse.response?.copy(
+                        text = messageResponse.response?.text + processRequestEnvironment.message.version
+                    )
+                )
+            }
+
+            request {
+                response {
+                    text = "test after handel, version alice: "
+                }
+            }
+        }
+    }.run()
 }
